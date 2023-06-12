bca407d71c609953879ae1dbb72eb364d38e78b5
Luis Gontarski <luisedgon@outlook.com>
2023-06-12 01:51:47 -0300

    Ajuste de nome do usuário

--- Status.java
+++ Status.java
@@ -14,61 +14,17 @@ public class Status extends JPanel {
     private JLabel golpeDeSorteLabel;
     private JLabel chanceDeGolpeDeSorteLabel;
     private JLabel pontuacaoLabel;
-    private String nomeJogador = JOptionPane.showInputDialog("Digite o seu nome:");
+    private String nomeJogador = "Fazendeiro";
     private JLabel upgradeCompradosLabel;
     private ArrayList<Upgrade> upgradesComprados;
 
 
-
-        /*public void salvarDados() {
-        String arquivoDados = "dados_usuario.txt";
-        try {
-            FileWriter writer = new FileWriter(arquivoDados);
-            writer.write(getNomeJogador() + "\n");
-            writer.write(getDinheiro() + "\n");
-            writer.write(getDinheiroPorClique() + "\n");
-            writer.write(getChancheGolpeDeSorte() + "\n");
-
-            writer.close();
-            System.out.println("Dados salvos com sucesso!");
-        } catch (IOException e) {
-            System.out.println("Erro ao salvar os dados: " + e.getMessage());
-        }
-    }
-    public void carregarDados() {
-        String arquivoDados = "dados_usuario.txt";
-        try {
-            FileReader reader = new FileReader(arquivoDados);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-
-            setNomeJogador(bufferedReader.readLine());
-            setDinheiro(Integer.parseInt(bufferedReader.readLine()));
-            setDinheiroPorClique(Integer.parseInt(bufferedReader.readLine()));
-            setChanceDeGolpeDeSorte(Integer.parseInt(bufferedReader.readLine()));
-
-            bufferedReader.close();
-            System.out.println("Dados carregados com sucesso!");
-        } catch (IOException e) {
-            System.out.println("Erro ao carregar os dados: " + e.getMessage());
-        }
-    }*/
-
     public Status() {
 
-        nomeJogador = JOptionPane.showInputDialog("Digite o seu nome:");
-        // Remova a chamada do método Save.carregarDados(this) daqui
-
-        if (nomeJogador == null || nomeJogador.isEmpty()) {
-            nomeJogador = JOptionPane.showInputDialog("Digite o seu nome:");
-            Save.salvarDados(nomeJogador, dinheiro, dinheiroPorClique, chanceDeGolpeDeSorte, golpeDeSorte);
-        } else {
-            System.out.println("Nome do jogador carregado: " + nomeJogador);
-        }
 
         this.createStatusPanel();
         this.createUpgradesCompradosLabel();
 
-        // Mova a chamada do método Save.carregarDados(this) para cá
         Save.carregarDados(this);
     }
 
